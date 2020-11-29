b4c3e8a3dc67241b5a581af157a774907495bc61
csweetj <csweetj_9636@yahoo.co.jp>
2020-11-29 14:40:04 +0900

    タイマー機能作成

---
+++ app/javascript/timer.js
@@ -0,0 +1,85 @@
+function timer() {
+  let timer = document.getElementById('timer');
+  let min = document.getElementById('min');
+  let sec = document.getElementById('sec');
+  let reset = document.getElementById('reset');
+  let start = document.getElementById('start');
+
+  let startTime;
+  let timeLeft;
+  let timeToCountDown = 0;
+  let timerId;
+  let isRunning = false;
+
+  function updateTimer(t) {
+    let d = new Date(t);
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let timerString;
+    m = ('0' + m).slice(-2);
+    s = ('0' + s).slice(-2);
+    timerString = m + ':' + s; 
+    timer.textContent = timerString;
+    document.title = timerString;
+  }
+
+  function countDown() {
+    timerId = setTimeout(function() {
+      timeLeft = timeToCountDown - (Date.now() - startTime);
+      if (timeLeft < 0) {
+        isRunning = false;
+        start.textContent = 'Start';
+        clearTimeout(timerId);
+        timeLeft = 0;
+        timeToCountDown = 0;
+        updateTimer(timeLeft);
+        return;
+      }
+      updateTimer(timeLeft);
+      countDown();
+    }, 10);
+  }
+
+ start.addEventListener('click', () => {
+   if (isRunning === false) {
+     isRunning = true;
+     start.textContent = 'Stop';
+     startTime = Date.now();
+     countDown();
+   } else {
+     isRunning = false;
+     start.textContent = 'Start';
+     timeToCountDown = timeLeft;
+     clearTimeout(timerId);
+   }
+ });
+ 
+ min.addEventListener('click', () => {
+   if (isRunning === true) {
+     return;
+   }
+   timeToCountDown += 60 * 1000;
+   if (timeToCountDown >= 60 * 60 * 1000) {
+    timeToCountDown = 0;
+  }
+   updateTimer(timeToCountDown);
+ });
+ 
+ sec.addEventListener('click', () => {
+  if (isRunning === true) {
+    return;
+  }
+   timeToCountDown += 1000;
+   if (timeToCountDown >= 60 * 60 * 1000) {
+     timeToCountDown = 0;
+   }
+   updateTimer(timeToCountDown);
+ });
+ 
+ reset.addEventListener('click', () => {
+   timeToCountDown = 0;
+   updateTimer(timeToCountDown);
+ });
+}
+
+window.addEventListener('load', timer)
