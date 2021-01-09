47a9074b2f81a766f7bd83484c0994d79a7cad8e
csweetj <csweetj_9636@yahoo.co.jp>
2021-01-09 21:17:45 +0900

    トップページへ戻るボタン追加

--- app/javascript/Posts/jscroll.js
+++ app/javascript/Posts/jscroll.js
@@ -1,13 +1,17 @@
 $(window).on('scroll', function() {
   if(!document.URL.match(/tag_list/)){
     scrollHeight = $(document).height();
-      scrollPosition = $(window).height() + $(window).scrollTop();
-      if ( (scrollHeight - scrollPosition) / scrollHeight <= 0.05) {
-            $('.jscroll').jscroll({
-              contentSelector: '.jscroll',
-              nextSelector: 'a.next',
-              loadingHtml: '読み込み中'
-            });
+    scrollPosition = $(window).height() + $(window).scrollTop();
+    if ( (scrollHeight - scrollPosition) / scrollHeight <= 0.05) {
+      $('.jscroll').jscroll({
+        contentSelector: '.jscroll',
+        nextSelector: 'a.next',
+        loadingHtml: '読み込み中'
+      });
+      $('.pagetop').fadeIn("slow");
+    }
+    else {
+      $('.pagetop').fadeOut('slow');
     }
   }
 });
