eb848340f5ae41fe066851a5b0072b2362151a1f
csweetj <csweetj_9636@yahoo.co.jp>
2021-01-16 13:20:06 +0900

    ビューを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    
-    # いいね合計数取得
     @posts = @user.posts.with_attached_feature_images.includes(:likes, :comments, :taggings).order('created_at DESC').page(params[:page]).per(6)
+    # いいね合計数取得
     @likes_count = 0
     @posts.each do |post|
       @likes_count += post.likes.length
