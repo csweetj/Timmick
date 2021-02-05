ef6a76866789522b95441f9d83c31ef3fc6e43ac
csweetj <csweetj_9636@yahoo.co.jp>
2021-02-05 23:03:23 +0900

    ビュー・エラーメッセージを修正

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Timmick
 
     # デフォルト言語を日本語
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     # 日本タイムゾーン設定
     config.time_zone = 'Tokyo'
   end
