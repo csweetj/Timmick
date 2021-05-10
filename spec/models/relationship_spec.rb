bba499306db00bd56a33556dc4c3701aeb6a74fb
csweetj <csweetj_9636@yahoo.co.jp>
2021-05-10 21:19:17 +0900

    [Add]Relationshipモデル単体テスト

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:relationship) { FactoryBot.create(:relationship) }
+  describe '#create' do
+    context "保存できる場合" do
+      it "全ての情報が揃っていれば保存できる" do
+        expect(relationship).to be_valid
+      end
+    end
+
+    context "保存できない場合" do
+      it "follower_idがない場合は保存できない" do
+        relationship.follower_id = nil
+        relationship.valid?
+        expect(relationship.errors[:follower_id]).to include("を入力してください")
+      end
+
+      it "following_idがないの場合は保存できない" do
+        relationship.following_id = nil
+        relationship.valid?
+        expect(relationship.errors[:following_id]).to include("を入力してください")
+      end
+    end
+  end
 end
