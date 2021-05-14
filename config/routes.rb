f0965266cf5e09d4849f021aa7ada3db2ca3ead9
csweetj <csweetj_9636@yahoo.co.jp>
2021-05-14 22:21:25 +0900

    [Fix]本番環境用ゲストユーザーログイン修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   end
   # ゲストユーザー
   devise_scope :user do
-    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    get 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
   resources :relationships, only: %i[create destroy]
 
